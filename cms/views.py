4a6445cae01fde2654bda994480d3e435bf567f5
cgauci87 <cgauci@gmail.com>
2023-01-26 13:34:44 +0000

    added OrderViewSet for Model Ordder -- cms/views.py

--- cms/views.py
+++ cms/views.py
@@ -9,6 +9,8 @@ from shop.models import (
 )
 
 # ProductViewSet for Item model
+
+
 class ProductViewSet(ModelViewSet):
     # list, get, update/patch, delete
     model = Item
@@ -17,7 +19,7 @@ class ProductViewSet(ModelViewSet):
     # permission_classes = [permissions.DjangoModelPermissionsOrAnonReadOnly]
 
     def products(self, request, *args, **kwargs):
-        queryset = Item.objects.all().order_by("-created_at") 
+        queryset = Item.objects.all().order_by("-created_at")
         serializer = ProductSerializer(queryset, many=True)
         return Response(serializer.data)
 
@@ -26,7 +28,8 @@ class ProductViewSet(ModelViewSet):
     #     serializer = ProductSerializer(queryset, many=True) # a nested representation of list of items
     #     return Response(serializer.data) # return serialized data
 
-    @action(detail=False, methods=['DELETE']) # using the action decorator with the detail flagged as False to return a list of objects
+    # using the action decorator with the detail flagged as False to return a list of objects
+    @action(detail=False, methods=['DELETE'])
     def bulk_delete(self, request):
         ids = request.data
         # bulk deletion of multiple products
@@ -34,7 +37,7 @@ class ProductViewSet(ModelViewSet):
         return Response(status=200)
 
 
-# @user_passes_test(Account.is_admin)
+# OrderViewSet for Model Order
 class OrderViewSet(ModelViewSet):
     # list, get, update/patch, delete
     model = Order
@@ -42,5 +45,7 @@ class OrderViewSet(ModelViewSet):
     serializer_class = OrderSerializer
 
     def list(self, request, *args, **kwargs):
+        # order by created_at (recent created products will display first in the list)
         queryset = Order.objects.all().order_by("-created_at")
+        # return a list
         return Response(OrderSerializer(queryset, many=True).data)
