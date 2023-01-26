a4fc0d6195bea22a476d07dd57ad368a7e455d9c
cgauci87 <cgauci@gmail.com>
2023-01-26 07:14:33 +0000

    added Order Model -- shop/models.py

--- shop/models.py
+++ shop/models.py
@@ -1,17 +1,13 @@
 import uuid
-
-from django.conf import settings
 from django.db import models
 
 
 CATEGORY_CHOICES = (
-    ('', ''),
     ('Mushroom Kits', 'Mushroom Kits'),
     ('Mushroom Powder', 'Mushroom Powder'),
 )
 
 TAG_CHOICES = (
-    ('', ''),
     ('Featured', 'Featured'),
     ('Spring', 'Spring'),
     ('Summer', 'Summer'),
@@ -20,30 +16,38 @@ TAG_CHOICES = (
 )
 
 STATUS_CHOICES = (
+    # 'Order Created' status would be set automatically once order has been submitted through the checkout
     ('Order Created', 'Order Created'),
+    # Status would be updated manually to 'Order Processing' by Admin in CMS
     ('Order Processing', 'Order Processing'),
+    # Status would be updated manually to 'Order Ready' by Admin in CMS
     ('Order Ready', 'Order Ready'),
+    # Status would be updated manually to 'Order Delivery' by Admin in CMS
     ('Order Delivered', 'Order Delivered'),
 )
 
 CHECKOUT_CHOICES = (
     ('Guest', 'Guest'),
     ('Registered', 'Registered'),
-    ('Registered-OTF', 'Registered-OTF'),
+    ('Registered-OTF', 'Registered-OTF')
 )
 
 ADDRESS_CHOICES = (
+    # included Billing choice for future implementation, payment gateway integration
     ('B', 'Billing'),
     ('S', 'Shipping'),
 )
+
+# Order Model - used in the checkout process and to manage orders in CMS
 
 
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     checkout_type = models.CharField(max_length=20,
-                                    choices=CHECKOUT_CHOICES, default='Guest', null=True)
+                                     choices=CHECKOUT_CHOICES, default='Guest', null=True)  # including CHECKOUT_CHOICES
     order_status = models.CharField(max_length=20,
-                                    choices=STATUS_CHOICES, default='Order Created', null=True)
+                                    choices=STATUS_CHOICES, default='Order Created', null=True)  # including STATUS_CHOICES
+    # Generate a random id, unique order number using UUID (see below def)
     order_id = models.CharField(max_length=120, blank=True)
     qty = models.IntegerField(null=True, blank=True)
     price = models.FloatField(blank=True, default=0)
@@ -55,22 +59,24 @@ class Order(models.Model):
         max_length=25)
     email = models.EmailField(
         max_length=50)
-    address_id = models.CharField(max_length=120, blank=True)
-    address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
+    address_type = models.CharField(
+        max_length=1, default='S', choices=ADDRESS_CHOICES)  # including ADDRESS_CHOICES
     apartment_address = models.CharField(max_length=100)
     street_address = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
     phone_number = models.IntegerField()
 
     def __str__(self):
+        # generate string representations of the objects
         return f'{self.id} {self.item}'
 
+    # this decorator declare that it can be accessed like it's a regular property.
     @property
-    def full_name(self):
+    def full_name(self):  # returns a string with the user first and last name.
         return f'{self.first_name} {self.last_name}'
 
-    def _generate_order_id(self):
-        # Generate a random, unique order number using UUID
+    def generate_order_id(self):
+        # generate a random, unique order number using UUID
 
         return uuid.uuid4().hex.upper()
 
@@ -80,7 +86,7 @@ class Order(models.Model):
         # if it hasn't been set already.
 
         if not self.order_id:
-            self.order_id = self._generate_order_id()
+            self.order_id = self.generate_order_id()
         super().save(*args, **kwargs)
 
     def __str__(self):
