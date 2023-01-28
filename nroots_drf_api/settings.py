a91227034caf3c860093adcd2f19bc6b805f6302
cgauci87 <cgauci@gmail.com>
2023-01-28 22:06:34 +0000

    Updated tests

--- nroots_drf_api/settings.py
+++ nroots_drf_api/settings.py
@@ -256,6 +256,7 @@ SIMPLE_JWT = {
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
 
     "DEFAULT_PERMISSION_CLASSES": [
