8b68ad92f68fff40e3eccb1ca3b5e90af380ae6d
cgauci87 <cgauci@gmail.com>
2023-02-19 10:44:07 +0000

    recreated fieldsets for AccountAdmin -- users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Account
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
-admin.site.register(Account)
+
+class AccountAdmin(UserAdmin):
+    filter_horizontal = []
+    ordering = ['email']
+    list_display = ['email', 'first_name', 'last_name']
+    list_filter = ['is_active', 'is_staff', 'is_superuser']
+
+    fieldsets = (
+        ('User Information', {'fields': ('email', 'first_name', 'last_name', 'is_superuser', 'is_admin', 'is_staff', 'is_active', 'reset_password_token', 'password')},
+         ),
+    )
+
+
+admin.site.register(Account, AccountAdmin)
